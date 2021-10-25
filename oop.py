79cf9cbb550619fea5f42d34555a80e824b061d7
Avror <kozarakovca@gmail.com>
2021-10-25 12:25:47 +0300

    FirstCommit

--- oop.py
+++ oop.py
@@ -1,1 +1,31 @@
-1
+class Parentse:
+    def __init__(self, name="Я", age=1, haircolor="черный"):
+        self.age = age
+        self.name = name
+        self.haircolor = haircolor
+
+
+class Son(Parentse):
+    def push_ups(self):
+        print("Делаю отжимания")
+
+
+class Daughter(Parentse):
+
+    def abs(self):
+        print("качаю пресс")
+
+
+class NevedomayaIgrushka(Parentse):
+    def song(self):
+        print("пою когда нажимают кнопку на моем животе")
+
+
+Usman = Son("Усман", 45, "коричневый")
+Usman.push_ups()
+
+Kamila = Daughter("Камила", 19, "блонд")
+Kamila.abs()
+
+Dino = NevedomayaIgrushka("Дино", 0, "нету")
+Dino.song()
